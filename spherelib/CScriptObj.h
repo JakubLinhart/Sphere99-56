4997841fefb59e8af1475bb4e7ffe7c68a82353f
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-28 20:27:45 +0100

    TRIGRET_TYPE

--- spherelib/CScriptObj.h
+++ spherelib/CScriptObj.h
@@ -1,5 +1,19 @@
 #pragma once
 
+enum TRIGRET_TYPE	// trigger script returns.
+{
+	TRIGRET_RET_FALSE = 0,	// default return. (script might not have been handled)
+	TRIGRET_RET_TRUE = 1,
+	TRIGRET_RET_DEFAULT,	// we just came to the end of the script.
+	TRIGRET_ENDIF,
+	TRIGRET_ELSE,
+	TRIGRET_ELSEIF,
+	TRIGRET_RET_HALFBAKED,
+	TRIGRET_BREAK,
+	TRIGRET_CONTINUE,
+	TRIGRET_QTY
+};
+
 class CScriptObj
 {
 };

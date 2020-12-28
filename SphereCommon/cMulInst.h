e4f9c1ad065ebf364071046db5871c870bfea242
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-28 12:10:58 +0100

    Remove empty lines.

--- SphereCommon/cMulInst.h
+++ SphereCommon/cMulInst.h
@@ -1,227 +1,114 @@
 //
-
 // CMulinst.h
-
 // Copyright Menace Software (www.menasoft.com).
-
 //
 
-
-
 #ifndef _INC_CSPHEREINST_H
-
 #define _INC_CSPHEREINST_H
-
 #if _MSC_VER >= 1000
-
 #pragma once
-
 #endif // _MSC_VER >= 1000
 
-
-
 #include "../spherelib/cfile.h"
-
 #include "../spherelib/carray.h"
-
-
 
 #include "spheremul.h"
 
-
-
 ////////////////////////////////////////////////////////
 
-
-
 struct CMulInstall
-
 {
-
 	// "Software\\Menasoft\\Sphere"
-
 	// "Software\\Origin Worlds Online\\Ultima Online\\1.0"
-
 	// bool m_fFullInstall;	// Are all files avail ?
-
 private:
-
 	static LPCTSTR const sm_szFileNames[VERFILE_QTY+1];
 
-
-
 	CGString m_sPreferPath;	// Prefer path in which to choose the files. (look here FIRST)
-
 	CGString m_sExePath;		// Files that are installed. "ExePath"
-
 	CGString m_sInstCDPath;		// For files that may still be on the CD. "InstCDPath"
-
 public:
-
 	CGFile m_File[ VERFILE_QTY ];	// Get our list of files we need to access.
 
-
-
 public:
-
 	CGString GetFullExePath( LPCTSTR pszName = NULL ) const
-
 	{
-
 		return( CGFile::GetMergedFileName( m_sExePath, pszName ));
-
 	}
-
 	CGString GetFullExePath( VERFILE_TYPE i ) const
-
 	{
-
 		return( GetFullExePath( GetBaseFileName( i )));
-
 	}
-
 	CGString GetFullCDPath( LPCTSTR pszName = NULL ) const
-
 	{
-
 		return( CGFile::GetMergedFileName( m_sInstCDPath, pszName ));
-
 	}
-
 	CGString GetFullCDPath( VERFILE_TYPE i ) const
-
 	{
-
 		return( GetFullCDPath( GetBaseFileName( i )));
-
 	}
-
-
 
 public:
-
 	bool FindInstall();
-
 	VERFILE_TYPE OpenFiles( DWORD dwMask, DWORD dwMaskEx );
-
 	bool OpenFile( CGFile& file, LPCTSTR pszName, WORD wFlags );
-
 	bool OpenFile( VERFILE_TYPE i );
-
 	void CloseFiles();
 
-
-
 	static LPCTSTR GetBaseFileName( VERFILE_TYPE i );
-
 	CGFile* GetMulFile( VERFILE_TYPE i )
-
 	{
-
 		ASSERT( i<VERFILE_QTY );
-
 		return( &(m_File[i]));
-
 	}
-
-
 
 	// set the attributes of a specific file.
-
 	HRESULT SetMulFile( VERFILE_TYPE i, LPCTSTR pszName );
 
-
-
 	void SetPreferPath( LPCTSTR pszName )
-
 	{
-
 		m_sPreferPath = pszName;
-
 	}
-
 	CGString GetPreferPath( LPCTSTR pszName = NULL ) const
-
 	{
-
 		return( CGFile::GetMergedFileName( m_sPreferPath.IsEmpty() ? m_sExePath : m_sPreferPath, pszName ));
-
 	}
-
 	CGString GetPreferPath( VERFILE_TYPE i ) const
-
 	{
-
 		return( GetPreferPath( GetBaseFileName( i )));
-
 	}
-
-
 
 	void SetExePath( LPCTSTR pszName );
-
 	void SetInstCDPath( LPCTSTR pszName );
 
-
-
 	bool IsValidMulIndex( VERFILE_TYPE fileindex, DWORD id );
-
 	bool ReadMulIndex( VERFILE_TYPE fileindex, VERFILE_TYPE filedata, DWORD id, CMulIndexRec& Index );
-
 	bool ReadMulData( VERFILE_TYPE filedata, const CMulIndexRec& Index, void * pData );
-
 };
-
-
 
 extern CMulInstall g_MulInstall;
 
-
-
 ///////////////////////////////////////////////////////////////////////////////
 
-
-
 class CMulVerData : public CGSortedArray< CMulVersionBlock, const CMulVersionBlock&, HASH_INDEX>
-
 {
-
 	// Find version diffs to the files listed.
-
 	// Assume this is a sorted array of some sort.
-
 protected:
-
 	int CompareKey( HASH_INDEX dwIndex, const CMulVersionBlock& verdata ) const
-
 	{
-
 		HASH_INDEX dwID2 = verdata.GetHashCode();
-
 		return HASH_COMPARE( dwIndex, dwID2 );
-
 	}
-
 public:
-
 	const CMulVersionBlock* GetEntry( int i ) const
-
 	{
-
 		return( &ConstElementAt(i));
-
 	}
-
 	bool Load( CGFile& file );
-
 	bool FindVerDataBlock( VERFILE_TYPE type, DWORD id, CMulIndexRec& Index ) const;
-
 };
 
-
-
 extern CMulVerData g_MulVerData;
-
-
 
 #endif	// _INC_CSPHEREINST_H_
--- SphereCommon/cdsoundchat.cpp
+++ SphereCommon/cdsoundchat.cpp
@@ -1,75 +1,74 @@
-//
-// cDSoundChat.cpp
-// Copyright Menace Software (www.menasoft.com).
-// Use the direct sound to load and transmit the sounds.
-//
-
-#include "stdafx.h"
-#include "cdsound.h"
-#include "../spherelib/common.h"
-
-#if 0 // def SPHERE_CLIENT
-
-class CSphereSoundPeer : public CGSocket
-{
-	// Send/Receive sound to this socket.
-private:
-	DWORD m_dwIP;	// CSocketAddressIP
-	CSphereUID m_uid;	// the uid of the sound source.
-
-public:
-	void OnEvent( WORD wEvent, WORD wError );
-};
-
-class CSphereSoundCapture
-{
-	// IDirectSoundCapture
-	// LPDIRECTSOUNDCAPTURE m_pDS;
-	CGRefArray <CSphereSoundPeer> m_Peers;
-	int m_iMaxPeerConnects;	// max number of connected audio systems.
-	bool m_fMute;	// do not send out any audio.
-	int m_iSquelchLevel;	// don't send stuff under this level. (for .10 secs)
-public:
-	CSphereSoundCapture()
-	{
-		// m_pDS = NULL;
-		m_iMaxPeerConnects = 0;
-	}
-#if 0
-	bool IsOpen() const
-	{
-		return( m_pDS != NULL );
-	}
-#endif
-	void Init();
-	void OnEvent( SOCKET socket, WORD wEvent, WORD wError );
-};
-
-void CSphereSoundCapture::Open()
-{
-	// Turn the local record option on. (if we want it at all)
-
-	if ( ! m_iMaxPeerConnects )
-	{
-		// close down and return
-		return;
-	}
-
-}
-
-void CSphereSoundCapture::AddPeer( bool fSelectPriv, DWORD dwIP )
-{
-	// Add a peer to my group.
-	// fSelectPriv = They are part of my selected high priv group.
-	// Attempt to connect to it.
-
-}
-
-void CSphereSoundPeer::OnEvent( WORD wEvent, WORD wError )
-{
-
-
-}
-
-#endif
-
+//
+// cDSoundChat.cpp
+// Copyright Menace Software (www.menasoft.com).
+// Use the direct sound to load and transmit the sounds.
+//
+
+#include "stdafx.h"
+#include "cdsound.h"
+#include "../spherelib/common.h"
+
+#if 0 // def SPHERE_CLIENT
+
+class CSphereSoundPeer : public CGSocket
+{
+	// Send/Receive sound to this socket.
+private:
+	DWORD m_dwIP;	// CSocketAddressIP
+	CSphereUID m_uid;	// the uid of the sound source.
+
+public:
+	void OnEvent( WORD wEvent, WORD wError );
+};
+
+class CSphereSoundCapture
+{
+	// IDirectSoundCapture
+	// LPDIRECTSOUNDCAPTURE m_pDS;
+	CGRefArray <CSphereSoundPeer> m_Peers;
+	int m_iMaxPeerConnects;	// max number of connected audio systems.
+	bool m_fMute;	// do not send out any audio.
+	int m_iSquelchLevel;	// don't send stuff under this level. (for .10 secs)
+public:
+	CSphereSoundCapture()
+	{
+		// m_pDS = NULL;
+		m_iMaxPeerConnects = 0;
+	}
+#if 0
+	bool IsOpen() const
+	{
+		return( m_pDS != NULL );
+	}
+#endif
+	void Init();
+	void OnEvent( SOCKET socket, WORD wEvent, WORD wError );
+};
+
+void CSphereSoundCapture::Open()
+{
+	// Turn the local record option on. (if we want it at all)
+
+	if ( ! m_iMaxPeerConnects )
+	{
+		// close down and return
+		return;
+	}
+
+}
+
+void CSphereSoundCapture::AddPeer( bool fSelectPriv, DWORD dwIP )
+{
+	// Add a peer to my group.
+	// fSelectPriv = They are part of my selected high priv group.
+	// Attempt to connect to it.
+
+}
+
+void CSphereSoundPeer::OnEvent( WORD wEvent, WORD wError )
+{
+
+}
+
+#endif
+

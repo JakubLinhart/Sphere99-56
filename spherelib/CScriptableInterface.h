ddbf9f89f657759f6a6b4b77554d4eb29e1d5dbe
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-28 20:17:06 +0100

    Stub DECLARE_LISTREC_REF.

--- spherelib/CScriptableInterface.h
+++ spherelib/CScriptableInterface.h
@@ -1,5 +1,7 @@
 #pragma once
 
 #define CSCRIPT_CLASS_IMP1(TYPE,_PROPS,_METHODS,_HELP)
+#define DECLARE_LISTREC_REF(a)
+#define DECLARE_LISTREC_REF2(a)
 #define CSCRIPT_EXEC_DEF void Exec
 #define CSCRIPT_CLASS_DEF1 void Exec

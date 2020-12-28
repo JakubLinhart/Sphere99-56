11d76ed50e660cdb7836d1b7aa4694bc54807592
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-28 11:56:59 +0100

    Implement CGRect::NormalizeRect and CGRect::NormalizeRectMax.

--- spherelib/cregion.h
+++ spherelib/cregion.h
@@ -3,12 +3,42 @@
 struct CGRect			// Basic rectangle. (May not be on the map)
 {
 public:
-	int m_left;		// West	 x=0
-	int m_top;		// North y=0
-	int m_right;	// East	( NON INCLUSIVE !)
-	int m_bottom;	// South ( NON INCLUSIVE !)
+	int left;		// West	 x=0
+	int top;		// North y=0
+	int right;	// East	( NON INCLUSIVE !)
+	int bottom;	// South ( NON INCLUSIVE !)
 
 public:
-	int Width() const { return(m_right - m_left); }
-	int Height() const { return(m_bottom - m_top); }
+	int Width() const { return(right - left); }
+	int Height() const { return(bottom - top); }
+
+	virtual void NormalizeRect()
+	{
+		if (m_bottom < m_top)
+		{
+			int wtmp = m_bottom;
+			m_bottom = m_top;
+			m_top = wtmp;
+		}
+		if (m_right < m_left)
+		{
+			int wtmp = m_right;
+			m_right = m_left;
+			m_left = wtmp;
+		}
+		if ((m_map < 0) || (m_map >= 256)) m_map = 0;
+		if (!g_MapList.m_maps[m_map]) m_map = 0;
+	}
+
+	void NormalizeRectMax(int cx, int cy)
+	{
+		if (m_left < 0)
+			m_left = 0;
+		if (m_top < 0)
+			m_top = 0;
+		if (m_right > cx)
+			m_right = cx;
+		if (m_bottom > cy)
+			m_bottom = cy;
+	}
 };

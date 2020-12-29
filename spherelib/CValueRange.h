dbbdd8db3830b4f688fc7cdb7fdd44f20d8c25eb
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-29 11:31:40 +0100

    Stub CValueRangeByte and CValueCurveDef (55).

--- spherelib/CValueRange.h
+++ spherelib/CValueRange.h
@@ -34,3 +34,57 @@ public:
 		Init();
 	}
 };
+
+struct CValueRangeByte
+{
+	// Simple linearity
+public:
+	BYTE m_iLo;
+	BYTE m_iHi;
+public:
+	void Init()
+	{
+		m_iLo = 0;
+		m_iHi = 255;
+	}
+	int GetRange() const
+	{
+		return(static_cast<BYTE>(m_iHi - m_iLo));
+	}
+	int GetLinear(int iPercent) const
+	{
+		// ARGS: iPercent = 0-1000
+		return(static_cast<BYTE>(m_iLo) + IMULDIV(GetRange(), iPercent, 1000));
+	}
+	int GetRandom() const
+	{
+		return(static_cast<BYTE>(m_iLo) + Calc_GetRandVal(GetRange()));
+	}
+	int GetRandomLinear(int iPercent) const;
+	void v_Set(CGVariant& vVal);
+	void v_Get(CGVariant& vVal);
+
+public:
+	CValueRangeInt()
+	{
+		Init();
+	}
+};
+
+struct CValueCurveDef
+{
+	// Describe an arbitrary curve.
+	// for a range from 0.0 to 100.0 (1000)
+	// May be a list of probabilties from 0 skill to 100.0% skill.
+public:
+	CGTypedArray<int, int> m_aiValues;		// 0 to 100.0 skill levels
+public:
+	void Init()
+	{
+		m_aiValues.Empty();
+	}
+	int GetLinear(int iSkillPercent) const;
+	int GetChancePercent(int iSkillPercent) const;
+	int GetRandom() const;
+	int GetRandomLinear(int iPercent) const;
+};

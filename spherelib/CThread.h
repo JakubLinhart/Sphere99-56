ed55f23a8477162c4ea127a232a452798db80271
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-29 14:25:11 +0100

    Add THREAD_ENTRY_RET implementation from 99.

--- spherelib/CThread.h
+++ spherelib/CThread.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#ifdef _WIN32
+#define THREAD_ENTRY_RET void
+#else	// else LINUX
+#define THREAD_ENTRY_RET void *
+#endif
 class CThread	// basic multi tasking functionality.
 {
 };

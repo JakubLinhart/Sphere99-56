7b45e0cd81ac65f7fc2466959e59bec14444bcca
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-30 20:29:59 +0100

    Stub CScript methods.

--- spherelib/CScript.h
+++ spherelib/CScript.h
@@ -18,6 +18,21 @@ class CScriptPropX
 class CScript : public CFileText
 {
 public:
-	virtual bool ReadTextLine(bool fRemoveBlanks);	// looking for a section or reading strangly formated section. 
-	TCHAR* GetLineBuffer();
+	virtual bool ReadTextLine(bool fRemoveBlanks) { throw "not implemented"; } // looking for a section or reading strangly formated section. 
+	TCHAR* GetLineBuffer() { throw "not implemented"; }
+
+	LPCTSTR GetKey() { throw "not implemented"; }
+	LPCTSTR GetArgStr() { throw "not implemented"; }
+	CGVariant& GetArgVar() { throw "not implemented"; }
+	
+	bool FindTextHeader(LPCTSTR pszName) { throw "not implemented"; } // Find a section in the current script
+	bool FindNextSection() { throw "not implemented"; }
+	bool ReadKeyParse() { throw "not implemented"; }
+	bool ReadLine() { throw "not implemented"; }
+
+	void WriteSection(LPCTSTR pszSection, ...) { throw "not implemented"; }
+	void WriteKey(LPCTSTR lpszKey, LPCTSTR lpszVal) { throw "not implemented"; }
+	void WriteKeyInt(LPCTSTR lpszKey, int iValue) { throw "not implemented"; }
+	void WriteKeyDWORD(LPCTSTR lpszKey, DWORD iValue) { throw "not implemented"; }
+
 };

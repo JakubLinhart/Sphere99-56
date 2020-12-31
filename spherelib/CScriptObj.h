4e6ca11c33733bfa4d91c1ec4cf309c030276861
Jakub Linhart <jakub.linhart@gmail.com>
2020-12-31 18:09:23 +0100

    Define script execution constants.

--- spherelib/CScriptObj.h
+++ spherelib/CScriptObj.h
@@ -4,11 +4,22 @@
 #define CSCRIPT_PARSE_HTML 1
 #define CSCRIPT_PARSE_NBSP 2
 
+enum TRIGRUN_TYPE
+{
+	TRIGRUN_SECTION_EXEC,	// Execute this section (first line already read)
+	TRIGRUN_SECTION_TRUE,	// Execute this section
+	TRIGRUN_SECTION_FALSE,	// Ignore this section
+	TRIGRUN_SINGLE_EXEC,	// Execute just this line or blocked segment (first line already read)
+	TRIGRUN_SINGLE_TRUE,	// Execute just this line or blocked segment
+	TRIGRUN_SINGLE_FALSE	// Ignore just this line or blocked segment
+};
+
 enum TRIGRET_TYPE	// trigger script returns.
 {
 	TRIGRET_RET_FALSE = 0,	// default return. (script might not have been handled)
 	TRIGRET_RET_TRUE = 1,
 	TRIGRET_RET_DEFAULT,	// we just came to the end of the script.
+	TRIGRET_RET_VAL,
 	TRIGRET_ENDIF,
 	TRIGRET_ELSE,
 	TRIGRET_ELSEIF,

ebaf32a2d087c23c6debf26b0a058931cd5896db
Jakub Linhart <jakub.linhart@gmail.com>
2021-01-06 23:56:05 +0100

    Stub CScriptExecContext members.

--- spherelib/CScriptExecContext.h
+++ spherelib/CScriptExecContext.h
@@ -8,5 +8,17 @@ public:
 	static void InitFunctions() { throw "not implemented"; }
 
 public:
+	CGVariant m_vValRet;
+	CVarDefArray m_ArgArray;
+
+	CScriptExecContext(CScriptObj *pObj, CScriptConsole* pConsole) { throw "not implemented"; }
+
+public:
 	virtual HRESULT Function_Dispatch(LPCTSTR pszKey, CGVariant& vArgs, CGVariant& vValRet) { throw "not implemented"; }
-};
+	void SetBaseObject(CScriptObj* pObj) { throw "not implemented"; }
+	CScriptObj* GetBaseObject() const { throw "not implemented"; }
+	CScriptConsole* GetSrc() const { throw "not implemented"; }
+	void s_ParseEscapes(LPCTSTR pszBuf, DWORD dwFlags) { throw "not implemented"; }
+	HRESULT ExecuteCommand(LPCTSTR pszCmd) { throw "not implemented"; }
+	TRIGRET_TYPE ExecuteScript(CScript& script, TRIGRUN_TYPE type) { throw "not implemented"; }
+};	
